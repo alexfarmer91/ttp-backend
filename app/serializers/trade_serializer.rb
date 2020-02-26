ef3806cb40fe322db4ce79d69a6b44db1ce0c66b
github username <github email address>
2020-02-26 16:08:29 -0500

    renamed column

--- app/serializers/trade_serializer.rb
+++ app/serializers/trade_serializer.rb
@@ -1,4 +1,4 @@
 class TradeSerializer < ActiveModel::Serializer
-  attributes :id, :ticker, :quantity, :price, :buy, :type
+  attributes :id, :ticker, :quantity, :price, :buy, :trade_type
   has_one :user
 end

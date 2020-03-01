50bc03647a11f0d1e1759b5736c69e3f8884996f
github username <github email address>
2020-03-01 18:23:15 -0500

    delete error fix

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :users
 
   get "/tests", to: "tests#index"
+  delete "/portfolio_items", to: "portfolio_items#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
